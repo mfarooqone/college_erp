8049ce348d03b8e124b11ced80b7479b2cc55259
Muhammad Farooq Iqbal <mfarooqiqbal143@gmail.com>
2026-06-04 14:40:03 +0500

    WIP update README for security, icon

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'College ERP',
-    'version': '19.0.1.0.1',
+    'version': '19.0.1.0.2',
     'license': 'LGPL-3',
     'author': 'Najoom Al Thuraya',
     'website': 'https://althurayauae.com/',
